067c951056c65c018760f136501d070e9f150f3d
Krekoten Marjan <krekoten@intensol.com>
2010-03-24 11:14:40 +0200

    Passed hosts an IPs are now used as keys for result

--- lib/ip_info_db.rb
+++ lib/ip_info_db.rb
@@ -15,13 +15,15 @@ module Marjan
     # Lookups for IP, host, list of IPs, hosts or mixed
     # Allways returns a list of valid Location objects
     def lookup ip_or_host, time_zone = false
+      _ips = [ip_or_host]
+      _ips = ip_or_host if ip_or_host.kind_of? Array
       ip_or_host = ip_or_host.join(',') if ip_or_host.kind_of? Array
       raise ArgumentError, 'First argument should be String or Array' unless ip_or_host.kind_of? String
       
       result = self.class.get('', :query => {:ip => ip_or_host, :timezone => time_zone.to_s})['Locations']
       locations = {}
       result.each do |location|
-        locations[location['Ip']] = Location.new(location.inject({}) {|hash, (key, value)| hash[key.to_s.gsub(/[A-Z]/){|s| '_' + s.downcase}.gsub(/^_/, '')] = value; hash}) if location['Status'] == 'OK'
+        locations[_ips[location['Id'].to_i]] = Location.new(location.inject({}) {|hash, (key, value)| hash[key.to_s.gsub(/[A-Z]/){|s| '_' + s.downcase}.gsub(/^_/, '')] = value; hash}) if location['Status'] == 'OK'
       end
       locations
     end
